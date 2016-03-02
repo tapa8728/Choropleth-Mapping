8cf0b3d0c3fa126c26fd43534998b3fd379cee27
tapa8728 <tapa8728@colorado.edu>
2016-03-02 00:20:49 -0700

    first level parsing done

--- Actual Data/test_parse.py
+++ Actual Data/test_parse.py
@@ -1,15 +1,38 @@
-f = open("gfg_users_states.csv", "r")
-lines = f.readlines()
+#read files
+user_data = open("gfg_users_states.csv", "r")
+resp_data = open("gfg_personality_survey_questions.csv", "r")
+
+#write files
+f1 = open("user_dict.txt", "w")
+
+lines = user_data.readlines()
 
 count = 0
 full = 0
+user = {}		#dict to store {gfgid : state} >> dont store if state is null
+				#dict to store {gfgid : {q# : answer value ... (45 pairs)}}
 
 for l in lines:
 	full = full + 1
 	m = l.split(",")
 	if m[1] == '\n':
-		count+=1
+		count+=1	# dont add to user
+	else:
+		gfgid = int(m[0].replace("\"", ""))
+		val = m[1].replace("\n", "").replace("\"", "")
+		user[gfgid] = val 
 
-print "Number of records with null values", count
-print "Number of records in total", full
-print "Thus, number of records usable", (full-count)
+#print "User dictionary ---", user 
+f1.write(user)
+
+print "Null States", count
+print "Total records", full
+print "Thus, usable records", (full-count)
+
+k = resp_data.readlines()
+for line in k:
+	n = line.split(",")
+	gfg_id = int(n[0].replace("\"", ""))
+	while gfg_id in user.keys(): 
+		anslist[n[1]] = n[2]
+
