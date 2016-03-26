80e155e15bfefc6656986885da3a5c639df01302
tapa8728 <tapa8728@colorado.edu>
2016-03-26 14:42:23 -0600

    Data dumped in dictionary

--- Actual Data/test_parse.py
+++ Actual Data/test_parse.py
@@ -1,16 +1,18 @@
+from collections import defaultdict
+
 #read files
 user_data = open("gfg_users_states.csv", "r")
-resp_data = open("gfg_personality_survey_questions.csv", "r")
+resp_data = open("gfg_personality_survey_responses.csv", "r")
 
 #write files
 f1 = open("user_dict.txt", "w")
-
+f2 = open("user_anser_dict", "w")
 lines = user_data.readlines()
 
 count = 0
 full = 0
-user = {}		#dict to store {gfgid : state} >> dont store if state is null
-				#dict to store {gfgid : {q# : answer value ... (45 pairs)}}
+user = defaultdict(list)		#dict to store {gfgid : state} >> dont store if state is null
+anslist ={}		#nested dict within user to store {gfgid : {q# : answer value ... (45 pairs)}}
 
 for l in lines:
 	full = full + 1
@@ -23,16 +25,21 @@ for l in lines:
 		user[gfgid] = val 
 
 #print "User dictionary ---", user 
-f1.write(user)
+dic = user
+f1.write(str(dic))
+print user[1]
 
 print "Null States", count
 print "Total records", full
 print "Thus, usable records", (full-count)
-
+a = []
 k = resp_data.readlines()
 for line in k:
 	n = line.split(",")
 	gfg_id = int(n[0].replace("\"", ""))
-	while gfg_id in user.keys(): 
-		anslist[n[1]] = n[2]
+	if gfg_id in user.keys(): 	#(k,v) pair already present in dict, simple have to creeate a list and append to it. 
+		a = [(n[1], n[2])]	
+		user[gfg_id].append(a)
+		a = [] #reset it
 
+f2.write(str(user))
