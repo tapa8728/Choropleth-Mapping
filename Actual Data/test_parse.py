ebf31f6a060ae77759e63f0462067edfea20a809
tapa8728 <tapa8728@colorado.edu>
2016-03-27 17:01:58 -0600

    Both files merged into a nested dict

--- Actual Data/test_parse.py
+++ Actual Data/test_parse.py
@@ -4,6 +4,8 @@ from collections import defaultdict
 user_data = open("gfg_users_states.csv", "r")
 resp_data = open("gfg_personality_survey_responses.csv", "r")
 
+f1 = open("user_dict.txt", "w")
+
 lines_user = user_data.readlines()
 lines_resp = resp_data.readlines()
 m = []
@@ -11,14 +13,19 @@ k = []
 d = {}			#create a dic
 for l in lines_user:	#userid, states file
 	m=l.split(",")	
-	d[m[0]] = {'state' : m[1]}				# append an object to dictionary
-	print m
-	print d
-	for l2 in lines_resp:	#userid, questionid, answer file
-		k = l2.split(",")
-		if(m[0] == k[0]):
-			print k
-	break
+	if m[1] != '\n':	#do nothing if state is null
+		print m
+		d[m[0]] = {'state' : m[1]}				# append an object to dictionary
+		for l2 in lines_resp:	#userid, questionid, answer file
+			k = l2.split(",")
+			if(m[0] == k[0]):
+				if m[0] in d:
+						d[m[0]][k[1]]=k[2]	# if key found in d dictionary
+	if(m[0] == '"15"'):
+		break
+
+p = d
+f1.write(str(p))
 
 
 
