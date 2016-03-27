118c0d896bc9579c9069b48fd3f488e046d50d4e
tapa8728 <tapa8728@colorado.edu>
2016-03-27 16:27:45 -0600

    Started storing in a nested dict

--- Actual Data/test_parse.py
+++ Actual Data/test_parse.py
@@ -4,42 +4,23 @@ from collections import defaultdict
 user_data = open("gfg_users_states.csv", "r")
 resp_data = open("gfg_personality_survey_responses.csv", "r")
 
-#write files
-f1 = open("user_dict.txt", "w")
-f2 = open("user_anser_dict", "w")
-lines = user_data.readlines()
+lines_user = user_data.readlines()
+lines_resp = resp_data.readlines()
+m = []
+k = []
+d = {}			#create a dic
+for l in lines_user:	#userid, states file
+	m=l.split(",")	
+	d[m[0]] = {'state' : m[1]}				# append an object to dictionary
+	print m
+	print d
+	for l2 in lines_resp:	#userid, questionid, answer file
+		k = l2.split(",")
+		if(m[0] == k[0]):
+			print k
+	break
 
-count = 0
-full = 0
-user = defaultdict(list)		#dict to store {gfgid : state} >> dont store if state is null
-anslist ={}		#nested dict within user to store {gfgid : {q# : answer value ... (45 pairs)}}
 
-for l in lines:
-	full = full + 1
-	m = l.split(",")
-	if m[1] == '\n':
-		count+=1	# dont add to user
-	else:
-		gfgid = int(m[0].replace("\"", ""))
-		val = m[1].replace("\n", "").replace("\"", "")
-		user[gfgid] = val 
 
-#print "User dictionary ---", user 
-dic = user
-f1.write(str(dic))
-print user[1]
 
-print "Null States", count
-print "Total records", full
-print "Thus, usable records", (full-count)
-a = []
-k = resp_data.readlines()
-for line in k:
-	n = line.split(",")
-	gfg_id = int(n[0].replace("\"", ""))
-	if gfg_id in user.keys(): 	#(k,v) pair already present in dict, simple have to creeate a list and append to it. 
-		a = [(n[1], n[2])]	
-		user[gfg_id].append(a)
-		a = [] #reset it
 
-f2.write(str(user))
