5100feed8d12005b1f090f2550388fbb11851c47
tapa8728 <tapa8728@colorado.edu>
2016-03-27 17:36:26 -0600

    key value pairs of dictionary cleaned up

--- Actual Data/test_parse.py
+++ Actual Data/test_parse.py
@@ -15,15 +15,24 @@ for l in lines_user:	#userid, states file
 	m=l.split(",")	
 	if m[1] != '\n':	#do nothing if state is null
 		print m
-		d[m[0]] = {'state' : m[1]}				# append an object to dictionary
+		gfgid = m[0].replace("\"", "")					#can be put into a function int_cleanup()
+		state = m[1].replace("\n", "").replace("\"", "")	#can be put into a function string_cleanup()
+		d[gfgid] = {'state' : state}				# append an object to dictionary
+		print "gfgid:",gfgid," state:",state
 		for l2 in lines_resp:	#userid, questionid, answer file
 			k = l2.split(",")
+			#print k
 			if(m[0] == k[0]):
-				if m[0] in d:
-						d[m[0]][k[1]]=k[2]	# if key found in d dictionary
-	if(m[0] == '"15"'):
-		break
-
+				q_no = k[1].replace("\"", "")
+				ans = k[2].replace("\n", "").replace("\"", "")
+				#print "q_no:",q_no," ans:",ans
+				if gfgid in d:
+					#print "key found"
+					d[gfgid][q_no]=str(ans)	# if key found in d dictionary
+	# if(m[0] == '"1"'):
+	# 	break
+	break
+print d
 p = d
 f1.write(str(p))
 
