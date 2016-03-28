8c4c7fafc874537db438966ec373c64c51db1a6c
Tanvi Parikh <tapa8728@colorado.edu>
2016-03-28 17:22:57 -0600

    Reverse() - reverse scoring - successfuly implemented and tested

--- Actual Data/PersonalityOperations.py
+++ Actual Data/PersonalityOperations.py
@@ -41,6 +41,23 @@ class PersonalityOperations(object):
 		respData.close()
 
 	'''
+		Function R(Reverse Scoring) implemented to compute the O,C,E,A,N formulae
+	'''
+	def reverse(self,n):
+		rev = 0
+		if n == 1:
+			rev = 5
+		elif n == 2:
+			rev = 4
+		elif n == 3:
+			rev = 3
+		elif n == 4:
+			rev = 2
+		elif n == 5:
+			rev = 1
+		return rev
+
+	'''
 		Read from lists, clean-up code and merge into a nested dictionary (userDict)
 		userDict(key) is gfgid 
 		userDict(value) is a another dict ..
@@ -82,11 +99,21 @@ class PersonalityOperations(object):
 			# Calculate O,C,E,A,N values after this point and add those (key,value) pairs to the nested dictionary
 			# Only then move to the next user
 			print "dict ---> ", self.userDict
-			m = self.userDict[gfgid]
-			print "nested Dict --:   ", m
-			for k in m:
-				print "k is--", k, " and value is-", m[k]
-			 	#print "key is - ", k, " and value is - ", v
+			nD = self.userDict[gfgid]
+			print ""
+			print "nested Dict -- :   ", nD
+			# Openness = 5, 10, 15, 20, 25, 30, 35R, 40, 41R, 44
+			openVal = int(nD['5']) + int(nD['10']) + int(nD['15']) + int(nD['20'])+ int(nD['25']) + int(nD['30']) + int(nD['35']) + int(nD['40']) + int(nD['41']) + int(nD['44'])
+			print "Reverse of 10 --", self.reverse(int(nD['10'])), " and nD['10'] is ", nD['10']
+			print "Openness -- ", openVal
+			# Conscientiousness: 3, 8R, 13, 18R, 23R, 28, 33, 38, 43R
+			concVal =4
+			# Extraversion: 1, 6R, 11, 16, 21R, 26, 31R, 36
+			extraVal =4
+			# Agreeableness: 2R, 7, 12R, 17, 22, 27R, 32, 37R, 42
+			agreeVal =4
+			# Neuroticism: 4, 9R, 14, 19, 24R, 29, 34R, 39
+			neuroVal = 4
 			break
 	
 	'''
