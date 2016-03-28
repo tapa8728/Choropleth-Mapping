e974cd5128f5413dc379eee0c02e6758068d518c
tapa8728 <tapa8728@colorado.edu>
2016-03-27 19:14:26 -0600

    (Version 1.0) test_parse.py written in class format in PersonalityOperations.py

---
+++ Actual Data/PersonalityOperations.py.py
@@ -0,0 +1,53 @@
+class PersonalityOperations(object):
+	'''
+		Read 2 files Nested for loop to merge them into 1  Computation time ~ 11-12 mins 
+	'''
+	#super.__init__(self)
+
+	def __init__(self):
+		self.user_data = None
+		self.resp_data = None
+		self.d = {}	
+		
+	def readFile(self,fname1,fname2):
+		self.user_data = open(fname1, "r")
+		self.resp_data = open(fname2, "r")
+
+	def writeFile(self,fname1):
+		self.f1 = open(fname1, "w")
+
+	def readintoList(self):
+		self.lines_user = self.user_data.readlines()
+		self.lines_resp = self.resp_data.readlines()
+
+	def closeFile(self):
+		user_data.close()
+		resp_data.close()
+
+	def computation(self):
+		for l in self.lines_user:	#userid, states file
+			m=l.split(",")	
+			if m[1] != '\n':	#do nothing if state is null
+				print m
+				gfgid, state = self.m[0].replace("\"", ""), m[1].replace("\n", "").replace("\"", "")					
+				self.d[gfgid] = {'state' : state}				# append an object to dictionary
+				for l2 in self.lines_resp:	#userid, questionid, answer file
+					k = l2.split(",")
+					if(m[0] == k[0]):
+						q_no, ans = k[1].replace("\"", ""), k[2].replace("\n", "").replace("\"", "")
+						if gfgid in self.d:
+							self.d[gfgid][q_no]=str(ans)	# if key found in d dictionary
+			break
+			
+	def writeToFile(self):
+		self.f1.write(str(self.d))
+
+if __name__== "__main__":
+	Po = PersonalityOperations()
+	Po.readFile("gfg_users_states.csv","gfg_personality_survey_responses.csv")
+	Po.writeFile("user_dict.txt")
+	Po.readintoList()
+	Po.computation()
+	Po.writeToFile()
+else:
+	print "Wrong module imported."
--- Actual Data/test_parse.py
+++ Actual Data/test_parse.py
@@ -1,5 +1,7 @@
-from collections import defaultdict
-
+"""
+Read 2 files 
+Computation time: ~11-12 mins
+"""
 #read files
 user_data = open("gfg_users_states.csv", "r")
 resp_data = open("gfg_personality_survey_responses.csv", "r")
@@ -18,7 +20,6 @@ for l in lines_user:	#userid, states file
 		gfgid = m[0].replace("\"", "")					#can be put into a function int_cleanup()
 		state = m[1].replace("\n", "").replace("\"", "")	#can be put into a function string_cleanup()
 		d[gfgid] = {'state' : state}				# append an object to dictionary
-		print "gfgid:",gfgid," state:",state
 		for l2 in lines_resp:	#userid, questionid, answer file
 			k = l2.split(",")
 			if(m[0] == k[0]):
@@ -27,7 +28,6 @@ for l in lines_user:	#userid, states file
 				if gfgid in d:
 					d[gfgid][q_no]=str(ans)	# if key found in d dictionary
 	
-print d
 p = d
 f1.write(str(p))
 
