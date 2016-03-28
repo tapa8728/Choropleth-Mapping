4a6133bc688b6b5a4c8031e87076812cfbc6cf4a
tapa8728 <tapa8728@colorado.edu>
2016-03-27 18:05:19 -0600

    Version 1.0 for combining the states.csv and responses.csv

--- Actual Data/test_parse.py
+++ Actual Data/test_parse.py
@@ -21,17 +21,12 @@ for l in lines_user:	#userid, states file
 		print "gfgid:",gfgid," state:",state
 		for l2 in lines_resp:	#userid, questionid, answer file
 			k = l2.split(",")
-			#print k
 			if(m[0] == k[0]):
 				q_no = k[1].replace("\"", "")
 				ans = k[2].replace("\n", "").replace("\"", "")
-				#print "q_no:",q_no," ans:",ans
 				if gfgid in d:
-					#print "key found"
 					d[gfgid][q_no]=str(ans)	# if key found in d dictionary
-	# if(m[0] == '"1"'):
-	# 	break
-	break
+	
 print d
 p = d
 f1.write(str(p))
