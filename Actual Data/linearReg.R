97abed4f06b0f1d20a3ace974e542aff9e1b8f54
tapa8728 <tapa8728@colorado.edu>
2016-04-10 18:43:21 -0600

    R script updated

--- Actual Data/linearReg.R
+++ Actual Data/linearReg.R
@@ -1,3 +1,11 @@
-filecsv = read.csv(file="stateDict.csv", head=TRUE, sep=",")
+filecsv = read.csv(file="C:\\Users\\Tanvi Parikh\\Desktop\\Choropleth-Mapping\\Actual Data\\stateDict.csv", head=TRUE, sep=",")
 
 print(summary(filecsv))
+
+print("lets calculate linear regression")
+
+print("we only need - gender, age and O ")
+myvars = c("age", "gender", "O")
+data2 = filecsv[myvars]
+
+print(summary(data2))
