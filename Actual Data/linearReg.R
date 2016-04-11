207d9eff147e8a555e08f94899bcc5d31d233508
tapa8728 <tapa8728@colorado.edu>
2016-04-10 23:13:32 -0600

    R file updated to dump the residuals into a new file along with old data

--- Actual Data/linearReg.R
+++ Actual Data/linearReg.R
@@ -8,30 +8,35 @@ print("LINEAR REGRESSION FOR O")
 results_o = lm(O ~ age + gender, data=filecsv)
 #print(results_o)
 rs_o<-residuals(results_o)
-print(rs_o)
+#print(rs_o)
 
 print("LINEAR REGRESSION FOR C")
 results_c = lm(C ~ age + gender, data=filecsv)
 #print(results_c)
 rs_c<-residuals(results_c)
-print(rs_c))
+#print(rs_c))
 
 print("LINEAR REGRESSION FOR E")
 results_e = lm(E ~ age + gender, data=filecsv)
 #print(results_e)
 rs_e<-residuals(results_e)
-print(rs_e)
+#print(rs_e)
 
 print("LINEAR REGRESSION FOR A")
 results_a = lm(A ~ age + gender, data=filecsv)
 #print(results_a)
 rs_a<-residuals(results_a)
-print(rs_a)
+#print(rs_a)
 
 print("LINEAR REGRESSION FOR N")
 results_n = lm(N ~ age + gender, data=filecsv)
 #print(results_n)
 rs_n<-residuals(results_n)
-print(rs_n)
+#print(rs_n)
 
+#append these values to new csv file
+
+x <- data.frame(filecsv, rs_o, rs_c, rs_e, rs_a, rs_n)
+
+write.table(x, file = "C:\\Users\\Tanvi Parikh\\Desktop\\Choropleth-Mapping\\Actual Data\\residual.csv", row.names=FALSE,col.names=TRUE,sep=",")
 
